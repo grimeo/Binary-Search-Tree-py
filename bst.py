712987bae56cabad4f7eb4061a4001b3635c3d7c
Romeo <romeogescolano@gmail.com>
2023-01-14 17:08:17 +0800

    add: function that finds the maximum value of the element

--- bst.py
+++ bst.py
@@ -58,3 +58,8 @@ class BinarySearchTreeNode:
             elements += self.right.to_pre_order_traversal()
             return elements
    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+    
