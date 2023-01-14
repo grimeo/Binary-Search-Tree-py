441fc42fbecdd06da41554c483dbdb00f518c3be
Romeo <romeogescolano@gmail.com>
2023-01-14 16:51:07 +0800

    add: search function for existing value

--- bst.py
+++ bst.py
@@ -18,3 +18,16 @@ class BinarySearchTreeNode:
             else:
                 self.right = BinarySearchTreeNode(data)
         
+    def search(self, val):
+        if self.data == val:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
