04ac3520e842d7d7a044d9f78e5366596c179245
Romeo <romeogescolano@gmail.com>
2023-01-14 17:12:31 +0800

    add: find_min function

--- bst.py
+++ bst.py
@@ -63,3 +63,7 @@ class BinarySearchTreeNode:
             return self.data
         return self.right.find_max()
     
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
