a65fcbd6eb7c243aae3aba8d11f7707530d2b50e
Romeo <romeogescolano@gmail.com>
2023-01-14 17:51:33 +0800

    add: dummy data for testing

--- bst.py
+++ bst.py
@@ -101,18 +101,17 @@ def build_tree(elements):
     return root
 
 if __name__ == '__main__':
-    
+    # delete example for numbers
     elements = [3,5,7,23,78,12,8,25,63]
     print(elements)
     elements_tree = build_tree(elements)
     elements_tree.delete(7)
     print(elements_tree.to_order_traversal())
     
+    # convert my name to binary search tree
     myname = ['R', 'O', 'M', 'E', 'O', 'G', 'E', 'S', 'C', 'O', 'L', 'A', 'N', 'O']
     myname_tree = build_tree(myname)
-
     print("My name in elements : ",myname)
-
     print("In order : ", myname_tree.to_order_traversal())
     print("Pre order: ",myname_tree.to_pre_order_traversal())
     print("Post order : ",myname_tree.to_post_order_traversal())
